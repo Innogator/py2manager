9c212b31ea38da88c22de17d4d4b3ed4a00239f6
BRYAN TANNER <BTANNE6@gmail.com>
2014-01-12 17:56:21 -0600

    updated routes

--- web2py/applications/py2manager/routes.py
+++ web2py/applications/py2manager/routes.py
@@ -25,13 +25,9 @@ possible_languages = read_possible_languages(abspath('applications', app))
 #NOTE! app - is an application based router's parameter with name of an
 #            application. E.g.'welcome'
 
-routers = {
-    app: dict(
-        default_language = possible_languages['default'][0],
-        languages = [lang for lang in possible_languages
-                           if lang != 'default']
+routers = dict(
+        BASE = dict(default_application='py2manager'),
     )
-}
 
 #NOTE! To change language in your application using these rules add this line
 #in one of your models files:
